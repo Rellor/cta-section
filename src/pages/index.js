202b9c86badc0cc16177df82975c77e2557de7a0
Rellor <basderoller@gmail.com>
2026-01-10 15:38:19 +0100

    Setup grid layout  based on content/design

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,31 @@ import ArrowRight from "@/components/icons/ArrowRight";
 export default function Home() {
   return (
     <div>
-      <main>
-        <p className="label">Label</p>
-        {/* In design it shows an h2, but here we can also use an h1 for SEO purposes */}
-        <h2>
-          Mandatory title about the{" "}
-          <span className="font-gt-alpina font-light">call-to-action here</span>
-        </h2>
-        <p>Optional body copy goes here.</p>
-        <button className="text-orangeRed text-normal">
-          Optional button <ArrowRight className="inline" />
-        </button>
+      <main className="w-full max-w-400 grid grid-cols-6 sm:grid-cols-12 gap-x-3 sm:gap-x-6 gap-y-20 p-5 sm:p-8 lg:p-16 mx-auto">
+        <div className="flex flex-col gap-6 items-center col-span-8 row-start-1 row-span-1 col-start-3">
+          <div className="text-center">
+            <p className="label">Label</p>
+            {/* In design it shows an h2, but here we can also use an h1 for SEO purposes */}
+            <h2>
+              Mandatory title about the{" "}
+              <span className="font-gt-alpina font-light mr-2">
+                <br className="max-sm:hidden" />
+                call-to-action
+              </span>
+              here
+            </h2>
+          </div>
+          <p>Optional body copy goes here.</p>
+          <button className="text-orangeRed text-normal w-fit">
+            Optional button <ArrowRight className="inline" />
+          </button>
+        </div>
+        <div className="bg-black col-span-6 aspect-4/3 row-start-2 row-span-1 rounded-sm relative">
+          <p className="absolute left-0 bottom-0 text-white">title</p>
+        </div>
+        <div className="bg-black col-span-6 aspect-4/3 row-start-2 row-span-1 rounded-sm relative">
+          <p className="absolute left-0 bottom-0 text-white">title</p>
+        </div>
       </main>
     </div>
   );
