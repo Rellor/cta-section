fea344bfe4e3a0e10a713014299af8e83142411b
Rellor <basderoller@gmail.com>
2026-01-10 13:59:19 +0100

    Added colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ export default function Home() {
   return (
     <div>
       <main>
-        <p>Normal</p>
-        <p className="font-medium">Medium</p>
-        <p className="font-bold">
-          DemiBold <span className="font-gt-alpina font-light">met Alpina</span>
-        </p>
+        <p className="text-groenGrijs">Label</p>
+        <h1 className="font-bold text-text1">
+          Mandatory title about the{" "}
+          <span className="font-gt-alpina font-light">call-to-action here</span>
+        </h1>
+        <p className="text-text2">Optional body copy goes here.</p>
+        <button className="text-orangeRed">Optional button ARROW</button>
       </main>
     </div>
   );
