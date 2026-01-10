d6e0c0c0800e6a084e15ecc152a16bf451a80e5c
Rellor <basderoller@gmail.com>
2026-01-10 17:15:00 +0100

    Case sensitive image

--- src/components/atoms/imageButton/index.jsx
+++ src/components/atoms/imageButton/index.jsx
@@ -5,7 +5,7 @@ const ImageButton = ({ className }) => {
     // Could also be possible to add hover:scale-[1.01] transition-all duration-200 for a bit more feedback when hovering
     <button
       className={`bg-black aspect-video sm:aspect-3/4 lg:aspect-4/3  rounded-sm relative bg-cover bg-center cursor-pointer group ${className}`}
-      style={{ backgroundImage: "url('/images/image.png')" }}
+      style={{ backgroundImage: "url('/images/Image.png')" }}
     >
       <div className="absolute inset-0 bg-linear-to-b from-transparent from-50% to-[#141414] rounded-sm"></div>
       {/* Is it correct that the bottom is slightly more than left/right in the design? */}
